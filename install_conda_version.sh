b69a33149d84a22b19a9643988d5c6203bd68781
Shuo Zhang <zhangshuo19930709@gmail.com>
2019-07-22 03:05:42 +0200

    EXP_OFF_V0 on hpc final

--- install_conda_version.sh
+++ install_conda_version.sh
@@ -1,6 +1,6 @@
 pip install tensorflow==1.9.0
 pip install -e .
-pip install -r requirement.txt
+pip install -r requirement_conda.txt
 cd ~/anaconda3/envs/cse/
 pip install -e git+git@github.com:williamd4112/gym.git@dabf2bf548d6d87b7c1109ba3bff14bb43c52aa6#egg=gym
 pip install -e git+git@github.com:rll/rllab.git@ba78e4c16dc492982e648f117875b22af3965579#egg=rllab
