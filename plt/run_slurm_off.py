24f3b587f03ea805b9bb5291851f2f4710b157dc
Shuo Zhang <zhangshuo19930709@gmail.com>
2019-07-27 17:49:33 +0200

    already now

--- plt/run_slurm_off.py
+++ plt/run_slurm_off.py
@@ -36,7 +36,7 @@ if time_limit == "24:00:00":
         logdir = logdir+'-5M/'
     elif num_timesteps=='1e6':
         logdir = logdir+'-1M/'
-    else logdir = logdir+'-do-not-use/'
+    else: logdir = logdir+'-do-not-use/'
 else:
     logdir = logdir+'-120/'
 
