0ee4aba5b9cbcfeacff20450e4572a8874dd3e79
Shuo Zhang <zhangshuo19930709@gmail.com>
2019-07-27 12:37:22 +0200

    ready?

--- plot_f/plot_offline_mbl_all.py
+++ plot_f/plot_offline_mbl_all.py
@@ -10,8 +10,8 @@ def filt(results,name,name_2=''):
 def filt_or(results,name,name_2):
     ls=[r for r in results if name in r.dirname or name_2 in r.dirname]
     return ls
-def filt_or_or(results,name,name_2,name_3):
-    ls=[r for r in results if name in r.dirname or name_2 in r.dirname or name_3 in r.dirname]
+def filt_or_or_or(results,name,name_2,name_3,name_4):
+    ls=[r for r in results if name in r.dirname or name_2 in r.dirname or name_3 in r.dirname or name_4 in r.dirname]
     return ls
 def main():  
     parser = arg_parser()
@@ -21,18 +21,18 @@ def main():
     dirname = '~/Desktop/carla_sample_efficient/data/bk/bkup_EXP2_FINAL/'+args.extra_dir+args.env
     
     results = pu.load_results(dirname)
-    r_copos_nosil,r_trpo_nosil,r_ppo_nosil=filt(results,'copos-'),filt(results,'trpo-'),filt(results,'ppo-')
-    r_copos_sil,r_trpo_sil,r_ppo_sil=filt(results,'copos+sil-'),filt(results,'trpo+sil-'),filt(results,'ppo+sil-')
+    r_copos1_nosil,r_copos2_nosil,r_trpo_nosil,r_ppo_nosil=filt(results,'copos1-'),filt(results,'copos2-'),filt(results,'trpo-'),filt(results,'ppo-')
+    r_copos1_sil,r_copos2_sil,r_trpo_sil,r_ppo_sil=filt(results,'copos1+sil-'),filt(results,'copos2+sil-'),filt(results,'trpo+sil-'),filt(results,'ppo+sil-')
     r_mbl_sil=filt(results,'mbl+','sil-')
  #   r_mbl_nosil_tmp=[r for r in results if r not in r_mbl_sil]
-    r_mbl_nosil=filt_or_or(results,'mbl+copos-','mbl+trpo-','mbl+ppo-')
+    r_mbl_nosil=filt_or_or_or(results,'mbl+copos1-','mbl+copos2-','mbl+trpo-','mbl+ppo-')
 
-    r_copos_comp, r_trpo_comp, r_ppo_comp=filt_or(results,'mbl+copos','copos+sil'),filt_or(results,'mbl+trpo','trpo+sil'),filt_or(results,'mbl+ppo','ppo+sil')
+    r_copos1_comp, r_copos2_comp, r_trpo_comp, r_ppo_comp=filt_or(results,'mbl+copos1','copos1+sil'),filt_or(results,'mbl+copos2','copos2+sil'),filt_or(results,'mbl+trpo','trpo+sil'),filt_or(results,'mbl+ppo','ppo+sil')
     
-    dt={'copos_nosil':r_copos_nosil, 'trpo_nosil':r_trpo_nosil, 'ppo_nosil':r_ppo_nosil,
-        'copos_sil':r_copos_sil, 'trpo_sil':r_trpo_sil, 'ppo_sil':r_ppo_sil,
+    dt={'copos1_nosil':r_copos1_nosil,'copos2_nosil':r_copos2_nosil, 'trpo_nosil':r_trpo_nosil, 'ppo_nosil':r_ppo_nosil,
+        'copos1_sil':r_copos1_sil,'copos2_sil':r_copos2_sil, 'trpo_sil':r_trpo_sil, 'ppo_sil':r_ppo_sil,
         'mbl_nosil':r_mbl_nosil, 'mbl_sil':r_mbl_sil,
-        'copos_comp':r_copos_comp, 'trpo_comp':r_trpo_comp, 'ppo_comp':r_ppo_comp}
+        'copos1_comp':r_copos1_comp,'copos2_comp':r_copos2_comp, 'trpo_comp':r_trpo_comp, 'ppo_comp':r_ppo_comp}
 
     for name in dt:
         pu.plot_results(dt[name],xy_fn=pu.progress_mbl_vbest_xy_fn,average_group=True,name=name,split_fn=lambda _: '',shaded_err=True,shaded_std=False)
@@ -41,7 +41,7 @@ def main():
         plt.tight_layout()
         fig = plt.gcf()
         fig.set_size_inches(9, 7.5)
-        fig.savefig("/Users/zsbjltwjj/Desktop/carla_sample_efficient/plot_f/OFFLINE/"+args.env+'/'+name+'.pdf',format="pdf")
+        fig.savefig("/Users/zsbjltwjj/Desktop/carla_sample_efficient/plot_f/OFFLINE/"+args.extra_dir+args.env+'/'+name+'.pdf',format="pdf")
         if name=='mbl_nosil' or name=='mbl_sil':
             pu.plot_results(dt[name],xy_fn=pu.progress_default_entropy_xy_fn,average_group=True,name=name,split_fn=lambda _: '',shaded_err=True,shaded_std=False,legend_entropy=1)
             plt.xlabel('Number of Timesteps [M]')
@@ -49,7 +49,7 @@ def main():
             plt.tight_layout()
             fig = plt.gcf()
             fig.set_size_inches(9, 7.5)
-            fig.savefig("/Users/zsbjltwjj/Desktop/carla_sample_efficient/plot_f/OFFLINE/"+args.env+'/'+name+'_entropy.pdf',format="pdf")
+            fig.savefig("/Users/zsbjltwjj/Desktop/carla_sample_efficient/plot_f/OFFLINE/"+args.extra_dir+args.env+'/'+name+'_entropy.pdf',format="pdf")
 
     
 if __name__ == '__main__':
--- plot_f/plot_online_all.py
+++ plot_f/plot_online_all.py
@@ -15,9 +15,9 @@ def main():
     dirname = '~/Desktop/carla_sample_efficient/data/bk/bkup_EXP1_FINAL/'+args.extra_dir+args.env
     
     results = pu.load_results(dirname)
-    r_copos,r_trpo,r_ppo=filt(results,'copos'),filt(results,'trpo'),filt(results,'ppo')
+    r_copos1,r_copos2,r_trpo,r_ppo=filt(results,'copos1'),filt(results,'copos2'),filt(results,'trpo'),filt(results,'ppo')
     r_sil_n2=filt(results,'sil_n2_l0.001')
-    dt={'copos':r_copos, 'trpo':r_trpo, 'ppo':r_ppo, 'sil_slight':r_sil_n2}
+    dt={'copos1':r_copos1, 'copos2':r_copos2,'trpo':r_trpo, 'ppo':r_ppo, 'sil_slight':r_sil_n2}
 
     for name in dt:
         pu.plot_results(dt[name],xy_fn=pu.progress_default_xy_fn,average_group=True,split_fn=lambda _: '',shaded_err=True,shaded_std=False)
@@ -26,7 +26,7 @@ def main():
         plt.tight_layout()
         fig = plt.gcf()
         fig.set_size_inches(9, 7.5)
-        fig.savefig("/Users/zsbjltwjj/Desktop/carla_sample_efficient/plot_f/ONLINE/"+args.env+'/'+name+'.pdf', format='pdf')
+        fig.savefig("/Users/zsbjltwjj/Desktop/carla_sample_efficient/plot_f/ONLINE/"+args.extra_dir+args.env+'/'+name+'.pdf', format='pdf')
         if name=='sil_slight':
             pu.plot_results(dt[name],xy_fn=pu.progress_default_entropy_xy_fn,average_group=True,split_fn=lambda _: '',shaded_err=True,shaded_std=False,legend_entropy=1)
             plt.xlabel('Number of Timesteps [M]')
@@ -34,7 +34,7 @@ def main():
             plt.tight_layout()
             fig = plt.gcf()
             fig.set_size_inches(9, 7.5)
-            fig.savefig("/Users/zsbjltwjj/Desktop/carla_sample_efficient/plot_f/ONLINE/"+args.env+'/'+name+'_entropy.pdf', format="pdf")
+            fig.savefig("/Users/zsbjltwjj/Desktop/carla_sample_efficient/plot_f/ONLINE/"+args.extra_dir+args.env+'/'+name+'_entropy.pdf', format="pdf")
     
 if __name__ == '__main__':
     main()
